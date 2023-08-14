f11030a37a660a71cdf9e22f5e69070c41e7cfc1
dagobah <alejandrodagobah@gmail.com>
2023-08-14 11:46:24 -0500

    Initial Commit

---
+++ src/components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/global.css'
+
+
+export default function Layout({children}) {
+  return (
+    <div className='text-bold'>
+        <div>
+            {children}
+
+        </div>
+        
+      <p>HOLA</p>
+    </div>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+export default function Home() {
+  return (
+    <Layout>
+      <div className="text-amber-600">
+        HGOLA
+      </div>
+
+    </Layout>
+
+  )
+}

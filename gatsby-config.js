4a2dbde4cbb0f53c8ecfb7f76dc99725873f4f54
dagobah <alejandrodagobah@gmail.com>
2023-09-29 13:38:11 -0500

    ADD Google Adwords

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,9 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-2FFKYDLW7X", // Google Analytics / GA
+          "G-2FFKYDLW7X",
+          "AW-11323692064"
+          // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],

5e28c371c742a6de271c21b6118f0113d2ecc7d7
dagobah <alejandrodagobah@gmail.com>
2023-09-29 11:08:28 -0500

    FIX pixel Image

--- src/components/meta/metaPixel.jsx
+++ src/components/meta/metaPixel.jsx
@@ -22,11 +22,7 @@ const MetaPixel = () => {
             </script>
 
             <noscript id="facebook-pixel-image">
-                {
-                    `<img height="1" width="1" style="display:none"
-                    src="https://www.facebook.com/tr?id=872008711165403&ev=PageView&noscript=1"
-                    />`
-                }
+                <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=872008711165403&ev=PageView&noscript=1"/>
             </noscript>
             
         {/* <!-- End Meta Pixel Code --> */}
